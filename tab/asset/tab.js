4820f39e794b1fc03b5ece026e6181fcc86fa14d
DUY <alan.hany123@gmail.com>
2022-06-19 15:10:05 +0700

    upt html,css and js

--- tab/asset/tab.js
+++ tab/asset/tab.js
@@ -1,5 +1,6 @@
 const headerList = document.querySelectorAll(".tab-header__item");
 [...headerList].forEach((item) => item.addEventListener("click", handleTab));
 function handleTab(event) {
-    console.log(event.target)
+    [...headerList].forEach((item) => item.classList.remove("active"));
+    event.target.classList.add("active");
 } 
